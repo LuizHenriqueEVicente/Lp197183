8a9fda7d7bbce5ccef4ac913acbbe7dc08201b27
LuizHenriqueEVicente <lhevicente@unifesp.br>
2026-04-29 20:50:58 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -4,7 +4,7 @@ Nome        : Luiz Henrique Estrasulas Vicente
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 29/04/2026
-Objetivo    : assimilar quebra de barra e função de printf
+Objetivo    : Imprimir a mensagem "Hello World!" e em seguida o final de linha
 Aprendizado : quebra de barra e printf
 -------------------------------------------------------------------------- */
 #include <stdio.h>
