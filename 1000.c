080926ab6e0d359be051be4df0477d74506d096d
LuizHenriqueEVicente <lhevicente@unifesp.br>
2026-04-29 20:06:06 -0300

    Add initial C program to print 'Hello World!'

---
+++ 1000.c
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Luiz Henrique Estrasulas Vicente
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 29/04/2026
+Objetivo    : assimilar quebra de barra e função de printf
+Aprendizado : quebra de barra e printf
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+ 
+int main() {
+    
+    printf("Hello World!\n");
+ 
+    return 0;
+}
