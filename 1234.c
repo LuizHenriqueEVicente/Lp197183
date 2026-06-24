0f6cb5726f79664be8a8e47b1666724b34f83448
LuizHenriqueEVicente <lhevicente@unifesp.br>
2026-06-24 10:37:40 -0300

    Add header comments to 1234.c
    Added header comments with problem details and objectives.

--- 1234.c
+++ 1234.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Luiz Henrique Estrasulas Vicente
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 
+Objetivo    : imprimir um texto alternando apenas as letras entre maiusculas e minusculas
+Aprendizado : uso de nova biblioteca, função do FGETS, e melhor entendimento de: (!=)
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
