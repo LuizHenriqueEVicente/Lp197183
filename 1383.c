bf4426a3dc5c3d60bb775dc493f6f832a1453b41
LuizHenriqueEVicente <lhevicente@unifesp.br>
2026-06-23 23:18:28 -0300

    Add Sudoku validation logic in C
    Implement a recursive function to validate Sudoku grids based on row, column, and 3x3 subgrid checks.

---
+++ 1383.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define TAMANHO 9
+
+int conferir_sudoku(int (*matriz)[TAMANHO], int r){//valida o sudoku linha a linha.
+    if(r == TAMANHO) return 1;//caso base: se chegou a linha 9, todas as outras tem que estar corretas.
+    
+    int linha[10] = {0}, coluna[10] = {0}, quadrante[10] = {0};//preenche a matriz com 0 para habilitar a checagem.
+    
+    for(int x = 0; x < TAMANHO; x++){//confere as colunas da linha X atual. 
+        
+        int numb_linha = matriz[r][x];//linha r, coluna X
+        int numb_coluna = matriz[x][r];//linha x, coluna r.
+        
+        //formula para checar 3x3
+        int quad_linha = 3 * (r / 3) + (x /3);
+        int quad_coluna = 3 * (r % 3) + (x % 3);
+        int numb_quadrante = matriz[quad_linha][quad_coluna];
+        
+        if(linha[numb_linha]++ || coluna[numb_coluna]++ || quadrante[numb_quadrante]++){//se o numero ja apareceu, retorna 0.
+            return 0;
+        }
+    }
+    return conferir_sudoku(matriz, r + 1);//caso recursivo, pula á proxima linha.(r + 1).
+}
+
+int main(){
+    
+    int aux, matriz[TAMANHO][TAMANHO];
+    
+    if(scanf("%d", &aux) == 1){//casos de teste
+        for(int instancia = 1; instancia <= aux; instancia++){//processa cada instancia do sudoku
+            for(int i = 0; i < TAMANHO; i++){//controla qual linha esta sendo lida
+                for(int z = 0; z < TAMANHO; z++){//controla qual coluna esta sendo lida
+                    scanf("%d", &matriz[i][z]);
+                }
+            }printf("Instancia %d\n%s\n\n", instancia, conferir_sudoku(matriz, 0) ? "SIM" : "NAO");//imprime a instancia atual e chama a recursiva
+        }
+    }
+    
+    return 0;
+}
